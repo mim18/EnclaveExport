c13c1e1915dd88ca257e4213c0bc654f9986b297
mim18 <mim18@pitt.edu>
2024-09-30 08:25:04 -0400

    Update PilotExportDemographics.sql
    Pilot Export for ENACT Enclaves

--- oracle/PilotExportDemographics.sql
+++ oracle/PilotExportDemographics.sql
@@ -1,13 +1,12 @@
 --DEMOGRAPHICS_CSV
---create table pph_dem as select * from (
 WITH CTE_COHORT_PATIENT_SET AS (
-SELECT * FROM {crcschema}.qt_patient_set_collection ps
-  join {crcschema}.qt_query_master qm on qm.
-where qm.qt_query_master_id = 
-  result_instance_id = 82788
+FROM {crc_schema}.qt_query_instance q
+  join {crc_schema}.qt_query_result_instance r on r.query_instance_id = q.query_instance_id 
+  join {crc_schema}.qt_patient_set_collection ps on ps.result_instance_id = r.result_instance_id and result_type_id = 1
+  where  q.query_master_id = {query_master_id}
 )
 SELECT   
-TRANSLATE(TO_CHAR(a.PATIENT_NUM), '1234567890', '2546789013') as "PATIENT_NUM"
+      a.PATIENT_NUM as "PATIENT_NUM"
       ,to_char(a.BIRTH_DATE,'MM/DD/YYYY') as "BIRTH_DATE"
       ,to_char(a.DEATH_DATE,'MM/DD/YYYY') as "DEATH_DATE"
       ,a.SEX_CD as "GENDER"
@@ -20,8 +19,8 @@ TRANSLATE(TO_CHAR(a.PATIENT_NUM), '1234567890', '2546789013') as "PATIENT_NUM"
       ,a.STATECITYZIP_PATH as "STATE_CITY_ZIP"
       ,a.INCOME_CD as "INCOME"
       ,a.VITAL_STATUS_CD as "VITAL_STATUS"
-  FROM ncatsprod_crcdata.patient_dimension a, 
-  CTE_COHORT_PATIENT_SET c
+  FROM {crc_schema}.patient_dimension a, 
+  CTE_COHORT_PATIENT_SET c 
   where a.patient_num = c.patient_num;
-  );
+  
   
